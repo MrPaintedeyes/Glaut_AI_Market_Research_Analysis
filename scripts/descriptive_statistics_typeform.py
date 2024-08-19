55edf2017470bd7968cb3f6f4e905fbd366d391e
MrPaintedeyes <occhidipinti00@gmail.com>
2024-08-19 13:13:03 +0200

    Update descriptive_statistics_typeform.py

--- scripts/descriptive_statistics_typeform.py
+++ scripts/descriptive_statistics_typeform.py
@@ -23,7 +23,7 @@ def compute_descriptive_stats(df, columns):
     return stats
 
 # Variables of interest
-variables = ['RATING', 'COMPL_TIME_SEC', '# words / respondent', '# themes / respondent', '# total followups / respondent']
+variables = ['RATING', '# words / respondent', '# themes / respondent', '# total followups / respondent']
 
 # Compute the descriptive statistics
 descriptive_stats = compute_descriptive_stats(conditioned_data, variables)
