969506069742d0684678b194e00d9fb9b0028101
MrPaintedeyes <occhidipinti00@gmail.com>
2024-08-19 02:33:20 +0200

    Update main.py

--- scripts/main.py
+++ scripts/main.py
@@ -2,9 +2,14 @@ from assess_themes_by_voice import assess_themes_by_voice
 from assess_themes_by_followup import assess_themes_by_followup
 from assess_rating_by_voice import assess_rating_by_voice
 from assess_rating_by_followup import assess_rating_by_followup
-
+from descriptive_statistics_glaut import descriptive_statistics_glaut
+from descriptive_statistics_typeform import descriptive_statistics_typeform
 def main():
-    data_path = r"C:user_file_path\synthetic_dataset.csv"
+    data_path = r"user_file_path\synthetic_dataset.csv"
+
+    print("Performing descriptive statistics...")
+    descriptive_statistics_glaut(data_path)
+    descriptive_statistics_typeform(data_path)
     
     print("Assessing themes by voice...")
     assess_themes_by_voice(data_path)
