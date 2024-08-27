90bdfd72b6fc17f8fb00b368a330e3a8142b585f
MrPaintedeyes <occhidipinti00@gmail.com>
2024-08-27 17:04:13 +0200

    Update main.py

--- scripts/main.py
+++ scripts/main.py
@@ -4,6 +4,8 @@ from assess_rating_by_voice import assess_rating_by_voice
 from assess_rating_by_followup import assess_rating_by_followup
 from descriptive_statistics_glaut import descriptive_statistics_glaut
 from descriptive_statistics_typeform import descriptive_statistics_typeform
+from assess_quality_scores_by_voice import assess_quality_scores_by_voice
+from assess_quality_scores_by_followup import assess_quality_scores_by_followup
 def main():
     data_path = r"user_file_path\synthetic_dataset.csv"
 
@@ -22,6 +24,12 @@ def main():
     
     print("Assessing rating by followup...")
     assess_rating_by_followup(data_path)
+
+    print("Assessing quality scores by voice...")
+    assess_quality_scores_by_voice(data_path)
+
+    print("Assessing quality scores by followup...")
+    assess_quality_scores_by_followup(data_path)
 
 if __name__ == "__main__":
     main()
